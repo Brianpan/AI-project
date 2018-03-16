7c0ad9c0f12e9957bc6ab18b34067070b5c4c713
Brianpan <b97705021@ntu.edu.tw>
2018-03-16 12:07:42 -0700

    final ai

--- src/MyAI.py
+++ src/MyAI.py
@@ -41,10 +41,12 @@ class MyAI ( Agent ):
         self.is_wumpus = True
 
         self.is_turnback = False
+        self.turn_init = False
         self.turning_move = []
         self.next_turn_move = None
         self.next_position = None
         self.has_arrow = True
+        self.last_shoot = False
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
@@ -59,6 +61,11 @@ class MyAI ( Agent ):
 
         if breeze and self.current_pos[0] == 0 and  self.current_pos[1] == 0:
             return Agent.Action.CLIMB
+
+        if self.last_shoot and not scream and self.current_pos[0] == 0 and  self.current_pos[1] == 0:
+            return Agent.Action.CLIMB
+        else:
+            last_shoot = False
 
         # update boundary
         if bump:
@@ -86,9 +93,11 @@ class MyAI ( Agent ):
         # check stench
         if stench:
             self.stench_status[cur_x][cur_y] = MyAI.TrapStatus.TRAP
-            # if self.has_arrow:
-            #     self.has_arrow = False
-            #     return Agent.Action.SHOOT
+            
+            if self.has_arrow and cur_x == 0 and cur_y == 0:
+                self.has_arrow = False
+                self.last_shoot = True
+                return Agent.Action.SHOOT
         else:
             self.stench_status[cur_x][cur_y] = MyAI.TrapStatus.SAFE
         
@@ -100,12 +109,14 @@ class MyAI ( Agent ):
                     new_x = cur_x + m[0]
                     new_y = cur_y + m[1]
                     self.map_status[new_x][new_y] = MyAI.MoveStatus.SAFE
-
-        # update neighbors are danger or not
-        self.updateNeighbors()
-
+        
+        # kill Wumpus or not 
+        
         if scream:
             self.is_wumpus = False
+        
+        # update neighbors are danger or not
+        self.updateNeighbors()
 
         if self.is_turnback is True:
             return self.turnBack()
@@ -176,83 +187,15 @@ class MyAI ( Agent ):
 
     # start tp turn back 
     def turnBack( self ):
-        if self.current_pos[0] == 0 and self.current_pos[1] == 0:
+        if not self.turn_init:
+            self.next_position = (0, 0)
+            self.turn_init = True
+            self.turning_move = self.calculateMoves()
+        if len(self.turning_move) > 0:
+            next_move = self.turning_move.pop(0)
+            return next_move
+        else:
             return Agent.Action.CLIMB
-
-        self.next_position = (0, 0)
-        
-        if not self.turning_move:
-            x = self.current_pos[0]
-            y = self.current_pos[1]
-
-            # check
-            if x - 1 >= 0 and self.isSafe(x-1, y):
-                if self.current_face == MyAI.FaceStatus.RIGHT:
-                    self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                elif self.current_face == MyAI.FaceStatus.LEFT:
-                    self.turning_move = [Agent.Action.FORWARD]
-                elif self.current_face == MyAI.FaceStatus.UP:
-                    self.turning_move = [Agent.Action.TURN_LEFT, Agent.Action.FORWARD]
-                else:
-                    self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                self.next_turn_move = [x-1, y]
-
-            elif y-1 >=0 and self.isSafe(x, y-1):
-                if self.current_face == MyAI.FaceStatus.RIGHT:
-                    self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                elif self.current_face == MyAI.FaceStatus.LEFT:
-                    self.turning_move = [Agent.Action.TURN_LEFT, Agent.Action.FORWARD]
-                elif self.current_face == MyAI.FaceStatus.UP:
-                    self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                else:
-                    self.turning_move = [Agent.Action.FORWARD]
-                self.next_turn_move = [x, y-1]
-
-            else:
-                # should do more smart
-                if x-1>=0 and y-1 >=0:
-                    hx = self.hazardRate(x-1, y)
-                    hy = self.hazardRate(x, y-1)
-                    if hx > hy:
-                        move_direction_y = True
-                    else:
-                        move_direction_y = False
-                elif x-1 < 0:
-                    move_direction_y = True
-                else:
-                    move_direction_y = False
-
-                if move_direction_y is False:
-                    if self.current_face == MyAI.FaceStatus.RIGHT:
-                        self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                    elif self.current_face == MyAI.FaceStatus.LEFT:
-                        self.turning_move = [Agent.Action.FORWARD]
-                    elif self.current_face == MyAI.FaceStatus.UP:
-                        self.turning_move = [Agent.Action.TURN_LEFT, Agent.Action.FORWARD]
-                    else:
-                        self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                    self.next_turn_move = [x-1, y]
-                else:
-                    if self.current_face == MyAI.FaceStatus.RIGHT:
-                        self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                    elif self.current_face == MyAI.FaceStatus.LEFT:
-                        self.turning_move = [Agent.Action.TURN_LEFT, Agent.Action.FORWARD]
-                    elif self.current_face == MyAI.FaceStatus.UP:
-                        self.turning_move = [Agent.Action.TURN_RIGHT, Agent.Action.TURN_RIGHT, Agent.Action.FORWARD]
-                    else:
-                        self.turning_move = [Agent.Action.FORWARD]
-                    self.next_turn_move = [x, y-1]
-                
-        next_move = self.turning_move.pop(0)
-        
-        # update face
-        self.updateFace(next_move)
-
-        if not self.turning_move:
-            self.prev_pos = self.current_pos
-            self.current_pos = self.next_turn_move
-
-        return next_move
 
     ## update face status ##
     def updateFace( self, move ):
@@ -298,7 +241,32 @@ class MyAI ( Agent ):
         return self.map_status[x][y] == MyAI.MoveStatus.SAFE
     
     def dist( self, p1, p2 ):
-        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+        diff_x = p2[0] - p1[0]
+        diff_y = p2[1] - p1[1]
+        turn_base = 0
+        
+        if self.current_face == MyAI.FaceStatus.UP:
+            if diff_x != 0:
+                turn_base += 1
+            if diff_y < 0:
+                turn_base += 2
+        elif self.current_face == MyAI.FaceStatus.DOWN:
+            if diff_x != 0:
+                turn_base += 1
+            if diff_y > 0:
+                turn_base += 2            
+        elif self.current_face == MyAI.FaceStatus.RIGHT:
+            if diff_x < 0:
+                turn_base += 2
+            if diff_y != 0:
+                turn_base += 1
+        else:
+            if diff_x > 0:
+                turn_base += 2
+            if diff_y != 0:
+                turn_base += 1
+
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) + turn_base
     
     # analyze next position to move
     def analyzeNextMove( self ):
@@ -322,7 +290,7 @@ class MyAI ( Agent ):
         self.move_candidates.remove(next_move[1])
 
         # too dangerous
-        if next_move[0][0] >= 0.6:
+        if next_move[0][0] > 0:
             self.is_turnback = True
 
         return next_move[1]
@@ -359,6 +327,43 @@ class MyAI ( Agent ):
         return hazards
 
     # move from one cell to another one
+    def generatePath(self, parent, dest_position, start_position):
+        position_list = [dest_position]
+        key = dest_position
+        
+        while parent[key] != start_position:
+            position_list.append(parent[key])
+            key = parent[key]
+
+        return position_list[::-1]
+    
+    def getPointMoves( self, cur_x, cur_y, dest_x, dest_y):
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        queue = [(cur_x, cur_y)]
+        parent = {}
+        visited = {}
+
+        while queue:
+            pt = queue.pop(0)
+            x = pt[0]
+            y = pt[1]
+            for m in moves:
+                cx = x+m[0] 
+                cy = y+m[1]
+                # check visited
+                if visited.get((cx,cy), False):
+                    continue
+                visited[(cx, cy)] = True
+
+                if cx >= 0 and cx < self.max_x and cy >= 0 and cy < self.max_y and self.map_status[cx][cy] == MyAI.MoveStatus.SAFE:
+                    parent[(cx, cy)] = (x, y)
+                    queue.append((cx, cy))
+
+                    if (cx == dest_x) and (cy == dest_y):
+                        dest_position = (dest_x, dest_y)
+                        
+                        return self.generatePath(parent, dest_position, (cur_x, cur_y))
+
     def calculateMoves( self ):
         x = self.current_pos[0]
         y = self.current_pos[1]
@@ -366,49 +371,23 @@ class MyAI ( Agent ):
         dest_y = self.next_position[1]
         c_face = self.current_face
 
-        delta_x = dest_x - x
-        delta_y = dest_y - y
         move_list = []
 
-        while delta_x != 0 or delta_y != 0:
-            moves = []
-            if delta_x > 0:
-                moves.append((1, 0))
-            elif delta_x < 0:
-                moves.append((-1, 0))
-
-            if delta_y > 0:
-                moves.append((0, 1))
-            elif delta_y < 0:
-                moves.append((0, -1))
-            ranking_list = []
-            for m in moves:
-                nx = x + m[0]
-                ny = y + m[1]
-                h = self.hazardRate(nx, ny)
-                ranking_list.append( [(nx, ny), h] )
-
-            ranking_list = sorted(ranking_list, key=lambda r: r[1])
-            next_move = ranking_list[0][0]
-
+        move_pts = self.getPointMoves(x, y, dest_x, dest_y)
+        for next_move in move_pts:
             move_list += self.stepMove(x, y, next_move[0], next_move[1], c_face)
-            
             # update face status
             if next_move[0] - x == 1:
                 c_face = MyAI.FaceStatus.RIGHT
-                delta_x -= 1
                 x += 1
             elif next_move[0] - x == -1:
                 c_face = MyAI.FaceStatus.LEFT
-                delta_x += 1
                 x -= 1
             elif next_move[1] - y == 1:
                 c_face = MyAI.FaceStatus.UP
-                delta_y -= 1
                 y += 1
             else:
                 c_face = MyAI.FaceStatus.DOWN
-                delta_y += 1
                 y -= 1
 
         return move_list
@@ -479,7 +458,8 @@ class MyAI ( Agent ):
                     if totalPt - safePt == 1:
                         self.map_status[evd_list[0][0]][evd_list[0][1]] = MyAI.MoveStatus.DANGER
                 
-                # if self.stench_status[cx][cy] == MyAI.TrapStatus.TRAP:
+                if self.breeze_status[x][y] == MyAI.TrapStatus.SAFE and self.is_wumpus == False:
+                    self.map_status[cx][cy] = MyAI.MoveStatus.SAFE
 
     # ======================================================================
     # YOUR CODE ENDS
